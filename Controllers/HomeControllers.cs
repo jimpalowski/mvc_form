a6f37e8009af3a5a1ff0a3214d09707c3f7a59ff
Jim Palowski and Noah Medoff <noah@>
2018-04-16 14:09:02 -0700

    initial commit create mvc file structures following the lesson and create forms

---
+++ Controllers/HomeControllers.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using FriendLetter.Models;
+
+namespace FriendLetter.Controllers
+{
+  public class HomeController : Controller
+  {
+    // [Route("/hello")]
+    // public string Hello() { return "Hello friend!"; }
+    // [Route("/goodbye")]
+    // public string Goodbye() { return "Goodbye friend."; }
+    [Route("/")]
+    // public ActionResult Letter()
+    // {
+    //   return View();
+    // }
+    public ActionResult Hello()
+    {
+      LetterVariable myLetterVariable = new LetterVariable();
+      myLetterVariable.SetRecipient("Eric");
+      myLetterVariable.SetSender("John");
+      return View(myLetterVariable);
+    }
+    [Route("/journal")]
+    public ActionResult Journal()
+    {
+      return View();
+    }
+    [Route("/form")]
+    public ActionResult Form()
+    {
+      return View();
+    }
+    [Route("/greeting_card")]
+    public ActionResult GreetingCard()
+    {
+      LetterVariable myLetterVariable = new LetterVariable();
+      myLetterVariable.SetRecipient(Request.Query["recipient"]);
+      myLetterVariable.SetSender(Request.Query["sender"]);
+      return View("Hello", myLetterVariable);
+    }
+  }
+}
